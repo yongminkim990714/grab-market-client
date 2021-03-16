5e98ab1fba3c7b0a2ee387deb2aa6be1d3240973
DESKTOP-FJIS0JA\min99 <min99713@naver.com>
2021-03-16 16:38:56 +0900

    Link 태그 적용"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import './App.css';
-import MainPageComponent from "./main/index.js"
+import "./App.css";
+import MainPageComponent from "./main/index.js";
+import { Switch, Route } from 'react-router-dom';
+import UploadPage from "./upload"
+import ProductPage from "./product"
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 
